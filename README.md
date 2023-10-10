b9bffafd71c20ad48a7fdd2261256b589a1590f7
David González <davidg@duckduckgo.com>
2023-10-10 14:54:50 +0200

    added actions and updated README

--- README.md
+++ README.md
@@ -1,23 +1,278 @@
-# Hello world javascript action
 
-This action prints "Hello World" or "Hello" + the name of a person to greet to the log.
+# Github-Asana action
 
-## Inputs
+This action integrates asana with github.
 
-### `who-to-greet`
+### Prerequisites
 
-**Required** The name of the person to greet. Default `"World"`.
+- Asana account with the permission on the particular project you want to integrate with.
+- For creating tasks in Asana, you must provide the Asana project where the issues will be added to.
+- For adding PR link to Asana, you must provide the task url in the PR description.
 
-## Outputs
+## Required Inputs
 
-### `time`
+### `asana-pat`
 
-The time we greeted you.
+**Required** Your public access token of asana, you can find it in [asana docs](https://developers.asana.com/docs/#authentication-basics).
 
-## Example usage
+### `action`
+
+**required** The action to be performed. Possible values are
+* `create-asana-issue-task` to create a task based on the Github Issue
+* `notify-pr-approved` to add a comment to the Asana task when the PR has been approved
+* `notify-pr-merged` to complete the Asana task when a PR has been merged
+* `check-pr-membership` checks the PR sender membership in the organisation that owns the repo
+* `add-asana-comment` adds a comment to the Asana task with the link to the Pull Request
+* `add-task-asana-project` adds a task to a project / section in Asana
+* `create-asana-pr-task` to create a task in Asana based on the Github Pull Request 
+* `get-latest-repo-release` to find the latest release version of a Github Repository
+* `create-asana-task` to create a task in Asana 
+
+### Create Asana task from Github Issue
+When a Github Issue has been added, it will create an Asana task with the Issue title, description and link.
+### `asana-project`
+
+**Required** The Asana project ID where the new task will be added i.e ASANA PROJECT: https://app.asana.com/0/1174433894299346
+
+#### Example Usage
 
 ```yaml
-uses: actions/hello-world-javascript-action@e76147da8e5c81eaf017dede5645551d4b94427b
-with:
-  who-to-greet: 'Mona the Octocat'
+on:
+  issues:
+    types: [opened, reopened]
+
+jobs:
+  sync:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: duckduckgo/native-github-asana-sync@v1.0
+        with:
+          asana-pat: 'Your PAT'
+          asana-project: 'Asana Project Id'
+          action: 'create-asana-issue-task'
+```
+### Comment on Asana task when PR has been reviewed
+When a Pull Request has been reviewed, it will look for an Asana task in the PR description and comment on it.
+### `trigger-phrase`
+
+**Required** Prefix before the task i.e ASANA TASK: https://app.asana.com/1/2/3/.
+
+#### Example Usage
+
+```yaml
+on:
+  pull_request_review:
+    types: [submitted]
+
+jobs:
+  pr-reviewed:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Update Asana task -> PR approved
+      - uses: duckduckgo/native-github-asana-sync@v1.0
+        with:
+          asana-pat: 'Your PAT'
+          trigger-phrase: 'Your Trigger Phrase'
+          action: 'notify-pr-approved'
+```
+
+### Complete Asana task when Github PR merged
+When a Github Pull Request has been closed, it will look for an Asana task in the PR description and close it.
+### `is-complete`
+**optional** Close the Asana task after Github PR merged when set to `true`
+#### Example Usage
+
+```yaml
+on:
+  pull_request:
+    types: [closed]
+
+jobs:
+  add-pr-merged-comment:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: duckduckgo/native-github-asana-sync@v1.0
+        if: github.event.pull_request.merged
+        with:
+          asana-pat: 'Your PAT'
+          trigger-phrase: 'Your Trigger Phrase'
+          action: 'notify-pr-merged'
+          is-complete: true
+```
+
+### Check membership of the PR author in the repo organisation
+When a Github Pull Request has been opened, it will check if the sender is a member of the organisation.
+This is one of the step of a bigger workflow, that process PRs differently depending if it's a community PR or not.
+**Required** Prefix before the task i.e ASANA TASK: https://app.asana.com/1/2/3/.
+### `github-pat`
+
+**Required** Github public access token
+#### Example Usage
+
+```yaml
+on:
+  pull_request:
+    types: [opened, reopened]
+
+jobs:
+  validate-pr:
+    name: Validate Pull Request
+    runs-on: ubuntu-latest
+    outputs:
+      output1: ${{ steps.step1.outputs.external }}
+    steps:
+      - name: Checking Pull Request sender membership
+        id: step1
+        uses: duckduckgo/native-github-asana-sync@v1.0
+        with:
+          github-pat: 'Your Github PAT'
+          action: 'check-pr-membership'
+```
+
+### Comment on Asana task when PR has been opened
+For PRs that are opened by members of the organisation, it will look for an Asana task in the PR description and comment on it with the PR link.
+### `trigger-phrase`
+**Required** Prefix before the task i.e ASANA TASK: https://app.asana.com/1/2/3/.
+
+### `is-pinned`
+**optional** Pinned the PR comment when set to `true`
+
+#### Sample PR Description
+``
+Asana Task: https://app.asana.com/0/1/2
+
+#### Example Usage
+
+```yaml
+on:
+  pull_request:
+    types: [opened, reopened]
+
+jobs:
+  sync:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Add comment in Asana task
+        uses: duckduckgo/native-github-asana-sync@v1.0
+        with:
+          asana-pat: 'Your PAT'
+          trigger-phrase: 'Your Trigger Phrase'
+          action: 'add-asana-comment'
+```
+
+### Add task to an Asana project
+Adds a task to an Asana project and section. The action will look for an Asana task in the PR description.
+### `trigger-phrase`
+**Required** Prefix before the task i.e ASANA TASK: https://app.asana.com/1/2/3/.
+### `asana-project`
+**Required** Id of the Asana project that the task will be added to. Task will be added to the top of the project.
+### `asana-section`
+**optional** Id of the Asana section in the Asana project. Task will be added to the top of the section.
+
+#### Example Usage
+```yaml
+on:
+  pull_request:
+    types: [opened, reopened]
+
+jobs:
+  sync:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Add Asana task to Project
+        uses: duckduckgo/native-github-asana-sync@v1.0
+        with:
+          trigger-phrase: 'Your Trigger Phrase'
+          asana-project: 'Asana Project Id'
+          asana-section: 'Asana Section Id'
+          action: 'add-task-asana-project'
+```
+
+### Create Asana task from Github Pull Request
+When a Github Pull Request has been added by a community contributor, it will create an Asana task with the Pull Request title, description and link.
+### `asana-project`
+
+**Required** The Asana project ID where the new task will be added i.e ASANA PROJECT: https://app.asana.com/0/1174433894299346
+
+#### Example Usage
+
+```yaml
+on:
+  issues:
+    types: [opened, reopened]
+
+jobs:
+  sync:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Create Asana task in Asana Project
+        uses: duckduckgo/native-github-asana-sync@v1.0
+        with:          
+          asana-project: 'Asana Project Id'
+          action: 'create-asana-pr-task'
+```
+
+### Find the lastest release version of a Github Repositoryu
+Finds the latest release version of a Github Repository.
+
+### `github-repository`
+**Required** Repository to check for the latest version.
+
+### `github-org`
+**Required** Organisation that owns the Repository.
+
+#### Example Usage
+
+```yaml
+on:
+  workflow_dispatch:
+
+jobs:
+  find-latest-version:
+    runs-on: ubuntu-latest
+    outputs:
+      version: ${{ steps.latest-release.outputs.version }}
+    steps:
+      - name: Find latest release of content scope scripts
+        id: latest-release
+        uses: duckduckgo/native-github-asana-sync@v1.0
+        with:
+          github-pat: 'Your Github PAT'
+          github-repository: 'Github Repository'
+          github-org: 'Github Organisation'
+          action: 'get-latest-repo-release'
+```
+
+
+### Create Asana task
+Creates an Asana task with the properties defined below.
+
+### `asana-project`
+**Required** The Asana project ID where the new task will be added i.e ASANA PROJECT: https://app.asana.com/0/1174433894299346
+### `asana-section`
+The Asana section ID in the Asana Project
+### `asana-task-name`
+**Required** Name of the Asana task
+### `asana-task-description`
+**Required** Description of the Asana task
+
+#### Example Usage
+
+```yaml
+on:
+  issues:
+    types: [opened, reopened]
+
+jobs:
+  sync:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Create Asana task in Asana Project
+        uses: duckduckgo/native-github-asana-sync@v1.0
+        with:          
+          asana-project: 'Asana Project Id'
+          asana-section: 'Asana Section Id'
+          asana-task-name: 'Asana Task Name'
+          asana-task-description: 'Asana Task Description'
+          action: 'create-asana-task'
 ```
---
+++ action.js
@@ -0,0 +1,368 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const octokit = require('@octokit/core');
+const asana = require('asana');
+
+function buildAsanaClient() {
+    const ASANA_PAT = core.getInput('asana-pat');
+    return asana.Client.create({
+        defaultHeaders: { 'asana-enable': 'new-sections,string_ids' },
+        logAsanaChangeWarnings: false
+    }).useAccessToken(ASANA_PAT).authorize();
+}
+
+function buildGithubClient(githubPAT){
+    return new octokit.Octokit({
+        auth: githubPAT
+      })
+}
+
+function findAsanaTasks(){
+    const
+        TRIGGER_PHRASE = core.getInput('trigger-phrase'),
+        PULL_REQUEST = github.context.payload.pull_request,
+        REGEX_STRING = `${TRIGGER_PHRASE} https:\\/\\/app.asana.com\\/(\\d+)\\/(?<project>\\d+)\\/(?<task>\\d+).*?`,
+        REGEX = new RegExp(REGEX_STRING, 'g');
+ 
+    console.info('looking for asana task link in body', PULL_REQUEST.body, 'regex', REGEX_STRING);
+    let foundTasks = [];
+    while((parseAsanaUrl = REGEX.exec(PULL_REQUEST.body)) !== null) {
+        const taskId = parseAsanaUrl.groups.task;
+        if (!taskId) {
+            core.error(`Invalid Asana task URL after trigger-phrase ${TRIGGER_PHRASE}`);
+            continue;
+        }
+        foundTasks.push(taskId);
+    }
+    console.info(`found ${foundTasks.length} tasksIds:`, foundTasks.join(','));
+    return foundTasks
+}
+
+async function createStory(client, taskId, text, isPinned) {
+    try {
+        return await client.stories.createStoryForTask(taskId, {
+            text: text,
+            is_pinned: isPinned,
+        });
+    } catch (error) {
+        console.error('rejecting promise', error);
+    }
+}
+
+async function createTask(client, name, description, comment, projectId) {
+    try {
+        client.tasks.createTask({name: name, 
+            notes: description, 
+            projects: [projectId],        
+            pretty: true})
+            .then((result) => {
+                console.log('task created', result.gid);
+                return createStory(client, result.gid, comment, true)
+            })
+    } catch (error) {
+        console.error('rejecting promise', error);
+    }
+}
+
+async function createIssueTask(){
+    const client = await buildAsanaClient();
+    const ISSUE = github.context.payload.issue;
+    const ASANA_PROJECT_ID = core.getInput('asana-project', {required: true});
+
+    console.info('creating asana task from issue', ISSUE.title);
+
+    const TASK_DESCRIPTION = `Description: ${ISSUE.body}`;
+    const TASK_NAME = `Github Issue: ${ISSUE.title}`;
+    const TASK_COMMENT = `Link to Issue: ${ISSUE.html_url}`;
+
+    return createTask(client, TASK_NAME, TASK_DESCRIPTION, TASK_COMMENT, ASANA_PROJECT_ID)
+}
+
+
+async function notifyPRApproved(){
+    const client = await buildAsanaClient();
+    const 
+        PULL_REQUEST = github.context.payload.pull_request,
+        TASK_COMMENT = `PR: ${PULL_REQUEST.html_url} has been approved`;
+
+    const foundTasks = findAsanaTasks()
+
+    const comments = [];
+    for (const taskId of foundTasks) {
+        const comment = createStory(client, taskId, TASK_COMMENT, false)
+        comments.push(comment)
+    }
+    return comments;
+}
+
+async function addTaskToAsanaProject(){
+    const client = await buildAsanaClient();
+
+    const projectId = core.getInput('asana-project', {required: true});
+    const sectionId = core.getInput('asana-section');
+    const taskId = core.getInput('asana-task-id', {required: true});
+
+    addTaskToProject(client, taskId, projectId, sectionId)     
+}
+
+async function addTaskToProject(client, taskId, projectId, sectionId){
+    if (!sectionId){
+        console.info('adding asana task to project', projectId);
+        try {
+            return await client.tasks.addProjectForTask(taskId, {
+                project: projectId,        
+                insert_after: null
+            });
+        } catch (error) {
+            console.error('rejecting promise', error);
+        }
+    } else {
+        console.info(`adding asana task to top of section ${sectionId} in project ${projectId}`);
+        try {
+            return await client.tasks.addProjectForTask(taskId, {
+                project: projectId                
+            })
+            .then((result) => {
+                client.sections.addTaskForSection(sectionId, {task: taskId})
+                .then((result) => {
+                    console.log(result);
+                });
+            });
+        } catch (error) {
+            console.error('rejecting promise', error);
+        }
+    }
+}
+
+async function addCommentToPRTask(){
+    const 
+        PULL_REQUEST = github.context.payload.pull_request,
+        TASK_COMMENT = `PR: ${PULL_REQUEST.html_url}`,
+        isPinned = core.getInput('is-pinned') === 'true';
+
+    const client = await buildAsanaClient();
+
+    const foundTasks = findAsanaTasks()
+
+    const comments = [];
+    for (const taskId of foundTasks) {
+        const comment = createStory(client, taskId, TASK_COMMENT, isPinned)
+        comments.push(comment)
+    }
+    return comments;
+}
+
+async function createPullRequestTask(){
+    const client = await buildAsanaClient();
+    const PULL_REQUEST = github.context.payload.pull_request;
+    const ASANA_PROJECT_ID = core.getInput('asana-project', {required: true});
+
+    console.info('creating asana task from pull request', PULL_REQUEST.title);
+
+    const TASK_DESCRIPTION = `Description: ${PULL_REQUEST.body}`;
+    const TASK_NAME = `Community Pull Request: ${PULL_REQUEST.title}`;
+    const TASK_COMMENT = `Link to Pull Request: ${PULL_REQUEST.html_url}`;
+
+    return createTask(client, TASK_NAME, TASK_DESCRIPTION, TASK_COMMENT, ASANA_PROJECT_ID)
+}
+
+async function completePRTask(){
+    const client = await buildAsanaClient();
+    const isComplete = core.getInput('is-complete') === 'true';
+
+    const foundTasks = findAsanaTasks()
+
+    const taskIds = [];
+    for(const taskId of foundTasks) {
+        console.info("marking task", taskId, isComplete ? 'complete' : 'incomplete');
+        try {
+            await client.tasks.update(taskId, {
+                completed: isComplete
+            });
+        } catch (error) {
+            console.error('rejecting promise', error);
+        }
+        taskIds.push(taskId);
+    }
+    return taskIds;
+}
+
+async function checkPRMembership(){
+    const 
+        PULL_REQUEST = github.context.payload.pull_request,
+        ORG = PULL_REQUEST.base.repo.owner.login,
+        USER = PULL_REQUEST.user.login;
+        HEAD = PULL_REQUEST.head.user.login
+
+        console.info(`PR opened/reopened by ${USER}, checking membership in ${ORG}`); 
+        if (HEAD === ORG){
+            console.log(author, `belongs to duckduckgo}`)
+            core.setOutput('external', false)
+          } else {
+            console.log(author, `does not belong to duckduckgo}`)
+            core.setOutput('external', true)
+          }          
+}
+
+async function getLatestRepositoryRelease(){
+    const 
+        GITHUB_PAT = core.getInput('github-pat', {required: true}),
+        githubClient = buildGithubClient(GITHUB_PAT),
+        ORG = core.getInput('github-org', {required: true}),
+        REPO = core.getInput('github-repository', {required: true});
+        
+    try {
+        await githubClient.request('GET /repos/{owner}/{repo}/releases/latest', {
+            owner: ORG,
+            repo: REPO,
+            headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+            }
+        }).then((response) => {
+            const version = response.data.tag_name
+            console.log(REPO, `latest version is ${version}`)
+            core.setOutput('version', version)
+        });
+    } catch (error) {
+        console.log(REPO, `can't find latest version ${error}`)
+        core.setFailed(`can't find latest version for ${REPO}`);
+    }
+
+}
+
+async function createAsanaTask(){
+    const 
+        projectId = core.getInput('asana-project', {required: true}),
+        sectionId = core.getInput('asana-section'),
+        taskName = core.getInput('asana-task-name', {required: true}),
+        taskDescription = core.getInput('asana-task-description', {required: true});
+
+    const client = await buildAsanaClient();
+
+    console.info('creating asana task', projectId);     
+    if (!sectionId){
+        try {
+            await client.tasks.create({            
+                projects: [projectId],
+                name: taskName,
+                notes: taskDescription,
+
+            }).then((response) => {
+                const taskId = response.gid
+                console.log(`task created with id ${taskId}`)
+                core.setOutput('taskId', taskId)
+            });
+        } catch (error) {
+            console.error('rejecting promise', error);
+        }
+    } else {
+        try {
+            await client.tasks.create({            
+                projects: [projectId],
+                memberships: [{project: projectId, section: sectionId}],
+                name: taskName,
+                notes: taskDescription,
+
+            }).then((response) => {
+                const taskId = response.gid
+                console.log(`task created with id ${taskId}`)
+                core.setOutput('taskId', taskId)
+            });
+        } catch (error) {
+            console.error('rejecting promise', error);
+        }
+    }        
+}
+
+async function addTaskPRDescription(){
+    const 
+        GITHUB_PAT = core.getInput('github-pat'),
+        githubClient = buildGithubClient(GITHUB_PAT),
+        ORG = core.getInput('github-org', {required: true}),
+        REPO = core.getInput('github-repository', {required: true}),
+        PR = core.getInput('github-pr', {required: true}),
+        projectId = core.getInput('asana-project', {required: true}),
+        taskId = core.getInput('asana-task-id', {required: true});   
+
+        githubClient.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+            owner: ORG,
+            repo: REPO,
+            pull_number: PR,
+            headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+            }
+            }).then((response) => {
+                console.log(response.data.body);
+                const body = response.data.body;
+                const asanaTaskMessage = `Task/Issue URL: https://app.asana.com/0/${projectId}/${taskId}/f`;        
+                const updatedBody = `${asanaTaskMessage} \n\n ----- \n${body}`;
+        
+                githubClient.request('PATCH /repos/{owner}/{repo}/pulls/{pull_number}', {
+                    owner: ORG,
+                    repo: REPO,
+                    pull_number: PR,
+                    body: updatedBody,
+                    headers: {
+                        'X-GitHub-Api-Version': '2022-11-28'
+                        }
+                    })
+                    .catch((error) => core.error(error));
+        
+            });
+      
+}
+
+async function action() {
+    const ACTION = core.getInput('action', {required: true});
+    console.info('calling', ACTION);
+
+    switch (ACTION) {
+        case 'create-asana-issue-task': {
+            createIssueTask();
+            break;
+        }
+        case 'notify-pr-approved': {
+            notifyPRApproved();
+            break;
+        }
+        case 'notify-pr-merged': {
+            completePRTask()
+            break;
+        }
+        case 'check-pr-membership': {
+            checkPRMembership();
+            break;
+        }
+        case 'add-asana-comment': {
+            addCommentToPRTask();
+            break;
+        }
+        case 'add-task-asana-project': {
+            addTaskToAsanaProject();
+            break;
+        }
+        case 'create-asana-pr-task': {
+            createPullRequestTask();
+            break;
+        }
+        case 'get-latest-repo-release': {
+            getLatestRepositoryRelease();
+            break;
+        }
+        case 'create-asana-task': {
+            createAsanaTask();
+            break;
+        }
+        case 'add-task-pr-description': {
+            addTaskPRDescription();
+            break;
+        }
+        default:
+            core.setFailed(`unexpected action ${ACTION}`);
+    }
+}
+
+module.exports = {
+    action,
+    default: action,
+};
