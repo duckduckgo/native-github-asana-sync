d2110a2ac0f14821691d636092128a8934549879
Anastasia Shuba <ashuba22@gmail.com>
2025-04-11 09:18:49 -0700

    update regex to match both Asana URL formats

--- dist/index.js
+++ dist/index.js
@@ -44,7 +44,7 @@ function findAsanaTasks(){
     const
         TRIGGER_PHRASE = core.getInput('trigger-phrase'),
         PULL_REQUEST = github.context.payload.pull_request,
-        REGEX_STRING = `${TRIGGER_PHRASE}\\s*https:\\/\\/app.asana.com\\/(\\d+)\\/(?<project>\\d+)\\/(?<task>\\d+).*?`,
+        REGEX_STRING = `${TRIGGER_PHRASE}\\s*https:\\/\\/app.asana.com\\/(\\d+)\\/((\\d+)\\/)?(project\\/)?(?<project>\\d+)(\\/task)?\\/(?<task>\\d+).*?`,
         SPECIFIED_PROJECT_ID = core.getInput('asana-project'),
         REGEX = new RegExp(REGEX_STRING, 'g');
 
